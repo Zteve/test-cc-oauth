c631754024b0fd7fcdb68868142a767e47e9f090
Ben Hale <bhale@pivotal.io>
2015-08-03 10:06:02 -0700

    Working OAuth2 Negotiation
    This commit completes a working OAuth2 negotiation and enumerates the
    list of applications visible to the user.  In order to run this
    application, three properties must be set:
    
    * HOST:                            the host being connected to
    * SECURITY_OAUTH2_CLIENT_USERNAME: the ERT username
    * SECURITY_OAUTH2_CLIENT_PASSWORD: the ERT password

--- src/main/java/io/pivotal/xd/test/Application.java
+++ src/main/java/io/pivotal/xd/test/Application.java
@@ -2,13 +2,39 @@ package io.pivotal.xd.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.OAuth2AutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.DefaultAccessTokenRequest;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = OAuth2AutoConfiguration.class)
 public class Application {
 
     public static void main(String[] args) throws Exception {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
         applicationContext.getBean(Sample.class).getSomething();
+    }
+
+    @Bean
+    public OAuth2RestTemplate oauth2RestTemplate(OAuth2ClientContext oauth2ClientContext,
+                                                 OAuth2ProtectedResourceDetails details) {
+        return new OAuth2RestTemplate(details, oauth2ClientContext);
+    }
+
+    @Bean
+    @ConfigurationProperties("security.oauth2.client")
+    OAuth2ProtectedResourceDetails oAuth2ProtectedResourceDetails() {
+        return new ResourceOwnerPasswordResourceDetails();
+    }
+
+    @Bean
+    public OAuth2ClientContext oauth2ClientContext() {
+        return new DefaultOAuth2ClientContext(new DefaultAccessTokenRequest());
     }
 }
--- src/main/java/io/pivotal/xd/test/Sample.java
+++ src/main/java/io/pivotal/xd/test/Sample.java
@@ -6,22 +6,45 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
 
+import java.util.List;
+import java.util.Map;
+
 @Component
 final class Sample {
 
-    @Value("${api.endpoint}")
-    private static String ENDPOINT;
+    private final String endpoint;
 
     private final RestOperations restOperations;
 
     @Autowired
-    Sample(RestOperations restOperations) {
+    Sample(@Value("${api.endpoint}") String endpoint, RestOperations restOperations) {
+        this.endpoint = endpoint;
         this.restOperations = restOperations;
     }
 
+    @SuppressWarnings("unchecked")
     public void getSomething() {
-        String result = this.restOperations.getForObject(String.format("%s/v2/apps", ENDPOINT), String.class);
-        System.out.println(String.format("\nResult is:\n%s\n", result));
+        Map<String, Object> result = this.restOperations.getForObject(getAppsEndpoint(), Map.class);
+
+        getResources(result).stream()
+                .map(this::getName)
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    private String getAppsEndpoint() {
+        return String.format("%s/v2/apps", this.endpoint);
+    }
+
+    @SuppressWarnings("unchecked")
+    private String getName(Map<String, Object> resource) {
+        Map<String, Object> entity = (Map<String, Object>) resource.get("entity");
+        return (String) entity.get("name");
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<Map<String, Object>> getResources(Map<String, Object> result) {
+        return (List<Map<String, Object>>) result.get("resources");
     }
 
 }
